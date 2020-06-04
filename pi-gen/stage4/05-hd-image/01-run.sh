6ac4d6e1eace704a99452c6500f1019861b6e46d
Max Narvaez <narvae1@stolaf.edu>
2020-06-04 07:08:47 +0000

    Update 01-run.sh

--- pi-gen/stage4/05-hd-image/01-run.sh
+++ pi-gen/stage4/05-hd-image/01-run.sh
@@ -1,17 +1,30 @@
 #!/bin/bash -e
 
+# Create HD directory
 install -m 777 -d "${ROOTFS_DIR}/usr/HD"
-install -m 777 files/hd-image.bash "${ROOTFS_DIR}/usr/HD/"
-install -m 777 files/PiTracker.bash "${ROOTFS_DIR}/usr/HD"
-ln -s "${ROOTFS_DIR}/usr/bin/hd-image.bash" "${ROOTFS_DIR}/usr/HD/hd-image"      # Looking back on this, this may not be correct, might need the .bash on the second part instead of the first.
-install -m 644 files/PiTracker.service "${ROOTFS_DIR}/lib/systemd/system/PiTracker.service"
+
+# Add version to HD directory
 install -m 666 files/version "${ROOTFS_DIR}/usr/HD"
 
+# Add hd-image.bash to HD directory
+install -m 777 files/hd-image.bash "${ROOTFS_DIR}/usr/HD"
+
+# Add PiTracker.bash to HD directory
+install -m 777 files/PiTracker.bash "${ROOTFS_DIR}/usr/HD"
+
+# Create a symbolic link so hd-image can be run without an explicit path
+ln -s "${ROOTFS_DIR}/usr/HD/hd-image.bash" "${ROOTFS_DIR}/usr/bin/hd-image"
+
+# Add PiTracker.service to systemd service files
+install -m 644 files/PiTracker.service "${ROOTFS_DIR}/lib/systemd/system/PiTracker.service"
+
+# Reload systemd and enable PiTracker service
 on_chroot << EOF
 systemctl daemon-reload
 systemctl enable PiTracker.service
 EOF
 
+# Add hd-admin user
 on_chroot << EOF
 if ! id -u hd-admin >/dev/null 2>&1; then
     adduser --disabled-password --gecos "" hd-admin
