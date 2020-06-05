2a08ab8072763ed66dccd14a7cbed845b695ede9
Max Narvaez <narvae1@stolaf.edu>
2020-06-05 15:42:17 +0000

    Update 01-run.sh

--- pi-gen/stage4/05-hd-image/01-run.sh
+++ pi-gen/stage4/05-hd-image/01-run.sh
@@ -2,6 +2,7 @@
 
 # Create HD directory
 install -m 777 -d "${ROOTFS_DIR}/usr/HD"
+echo "Created HD Directory"
 
 # Add version to HD directory
 install -m 666 files/version "${ROOTFS_DIR}/usr/HD"
@@ -11,16 +12,19 @@ install -m 777 files/hd-image.bash "${ROOTFS_DIR}/usr/HD"
 
 # Add PiTracker.bash to HD directory
 install -m 777 files/PiTracker.bash "${ROOTFS_DIR}/usr/HD"
+echo "Populated HD directory"
 
 # Create a symbolic link so hd-image can be run without an explicit path
 ln -s "${ROOTFS_DIR}/usr/HD/hd-image.bash" "${ROOTFS_DIR}/usr/bin/hd-image"
+echo "Created hd-image symlink"
 
 # Add PiTracker.service to systemd service files
 install -m 644 files/PiTracker.service "${ROOTFS_DIR}/lib/systemd/system/PiTracker.service"
+echo "Added PiTracker service"
 
 # Reload systemd and enable PiTracker service
 on_chroot << EOF
-systemctl daemon-reload
+# systemctl daemon-reload
 systemctl enable PiTracker.service
 EOF
 
