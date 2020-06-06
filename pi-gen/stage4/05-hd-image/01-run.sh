67b696d088fb27ae20dea17c6742c5230093b19c
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-06-06 00:56:48 -0500

    Install ansible in script

--- pi-gen/stage4/05-hd-image/01-run.sh
+++ pi-gen/stage4/05-hd-image/01-run.sh
@@ -1,38 +1,8 @@
 #!/bin/bash -e
 
-# Create HD directory
-install -m 777 -d "${ROOTFS_DIR}/usr/HD"
-echo "Created HD Directory"
 
-# Add version to HD directory
-install -m 666 files/version "${ROOTFS_DIR}/usr/HD"
+# Add hd-image user
 
-# Add hd-image.bash to HD directory
-install -m 777 files/hd-image.bash "${ROOTFS_DIR}/usr/HD"
-
-# Add PiTracker.bash to HD directory
-install -m 777 files/PiTracker.bash "${ROOTFS_DIR}/usr/HD"
-echo "Populated HD directory"
-
-if [ -f "${ROOTFS_DIR}/usr/bin/hd-image" ]
-then
-     rm "${ROOTFS_DIR}/usr/bin/hd-image"
-fi
-
-# Create a symbolic link so hd-image can be run without an explicit path
-ln -s "/usr/HD/hd-image.bash" "${ROOTFS_DIR}/usr/bin/hd-image"
-echo "Created hd-image symlink"
-
-# Add PiTracker.service to systemd service files
-install -m 644 files/PiTracker.service "${ROOTFS_DIR}/lib/systemd/system/PiTracker.service"
-echo "Added PiTracker service"
-
-# Reload systemd and enable PiTracker service
-on_chroot << EOF
-systemctl enable PiTracker.service
-EOF
-
-# Add hd-admin user
 on_chroot << EOF
 if ! id -u hd-admin >/dev/null 2>&1; then
     adduser --disabled-password --gecos "" hd-admin
@@ -41,5 +11,45 @@ echo "hd-admin:${ADMIN_PASS}" | chpasswd
 adduser hd-admin sudo
 EOF
 
+echo "Added hd-image user"
+
+
+# Prevent Welcome to Raspberry Pi window from appearing
+
 rm "${ROOTFS_DIR}/etc/xdg/autostart/piwiz.desktop"
-echo "Removed piwiz.desktop file (preventing Welcome to Raspberry Pi window from appearing)"
+echo "Removed piwiz.desktop file"
+
+
+# Install Ansible
+
+on_chroot << EOF
+pip3 install ansible
+EOF
+
+
+# HD Files
+
+install -m 777 -d "${ROOTFS_DIR}/usr/HD"
+echo "Created HD Directory"
+
+install -m 666 files/version "${ROOTFS_DIR}/usr/HD"
+install -m 777 files/hd-image.bash "${ROOTFS_DIR}/usr/HD"
+install -m 777 files/PiTracker.bash "${ROOTFS_DIR}/usr/HD"
+echo "Populated HD directory"
+
+if [ -f "${ROOTFS_DIR}/usr/bin/hd-image" ]
+then
+     rm "${ROOTFS_DIR}/usr/bin/hd-image"
+fi
+
+ln -s "/usr/HD/hd-image.bash" "${ROOTFS_DIR}/usr/bin/hd-image"
+echo "Created hd-image symlink"
+
+install -m 644 files/PiTracker.service "${ROOTFS_DIR}/lib/systemd/system/PiTracker.service"
+echo "Added PiTracker service"
+
+on_chroot << EOF
+systemctl enable PiTracker.service
+EOF
+echo "Enabled PiTracker to run at startup"
+
