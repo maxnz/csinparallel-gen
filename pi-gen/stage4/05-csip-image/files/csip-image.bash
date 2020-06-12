5f8c562eb00886d347c442d726bbbc443b9fedba
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-06-11 20:44:24 -0500

    Update csip-image.bash

--- pi-gen/stage4/05-csip-image/files/csip-image.bash
+++ pi-gen/stage4/05-csip-image/files/csip-image.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# csinparallel-image: a tool for managing the CSiP Image
+# csinparallel-image: a tool for managing the CSinParallel Image
 # Created by Max Narvaez
 
 IMAGEVER=`cat /usr/CSiP/version`
