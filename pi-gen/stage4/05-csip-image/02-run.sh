c9c8720a6dd6bc2493aedbc530a18a6f046626b5
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-06-13 14:30:39 -0500

    Clean up comments

--- pi-gen/stage4/05-csip-image/02-run.sh
+++ pi-gen/stage4/05-csip-image/02-run.sh
@@ -2,6 +2,8 @@
 
 #### Equivalent to ansible-pull for v3.0.1 (https://github.com/babatana/csinparallel-image/blob/master/updates/3.0.1.yaml)
 
+# Set static IP
+
 cat << EOF >> "${ROOTFS_DIR}/etc/dhcpcd.conf"
 interface eth0
 metric 302
@@ -13,22 +15,22 @@ nolink
 interface wlan0
 metric 202
 EOF
+echo "Set static IP"
 
-echo  Set static IP 
 
-#Add eth0 to DHCP server
+# Add eth0 to DHCP server
+
 sed -e 's/INTERFACESv4=""/INTERFACESv4="eth0"/g' -f "${ROOTFS_DIR}/etc/default/isc-dhcp-server" 
 echo "Add eth0 to DHCP server"
 
-#Configure DHCP server Part 1
+# Configure DHCP server
+
 sed -e 's/option domain-name \"example.org\";//g' -f "${ROOTFS_DIR}/etc/dhcp/dhcpd.conf"
 echo "Configure DHCP server Part 1"
 
-#Configure DHCP server Part 2
 sed -e 's/option domain-name-servers ns1.example.org, ns2.example.org;//g' -f "${ROOTFS_DIR}/etc/dhcp/dhcpd.conf"
 echo "Configure DHCP server Part 2"
 
-#Configure DHCP server Part 3
 cat << EOF >> "${ROOTFS_DIR}/etc/dhcp/dhcpd.conf"
 default-lease-time 600;
 max-lease-time 7200;
@@ -43,11 +45,15 @@ range 10.0.0.2 10.0.0.253;
 EOF
 echo "Configure DHCP server Part 3"
 
-#Configure DHCP server service
+
+# Configure DHCP server service
+
 install -m 644 files/isc-dhcp-server.service "${ROOTFS_DIR}/etc/systemd/system/isc-dhcp-server.service"
 echo "Configure DHCP server service"
 
-#Enable dhcp server
+
+# Enable dhcp server
+
 on_chroot << EOF
 systemctl enable isc-dhcp-server
 EOF
