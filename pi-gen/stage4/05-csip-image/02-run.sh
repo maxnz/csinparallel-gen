09634cc32c3f934104583281d733b4fe2a837ea3
GeorgiosKokalas <45497707+GeorgiosKokalas@users.noreply.github.com>
2020-06-13 12:46:45 -0500

    Update 02-run.sh
    added the setup static IP line code from 3.0.1.yaml

--- pi-gen/stage4/05-csip-image/02-run.sh
+++ pi-gen/stage4/05-csip-image/02-run.sh
@@ -2,3 +2,14 @@
 
 #### Equivalent to ansible-pull for v3.0.1 (https://github.com/babatana/csinparallel-image/blob/master/updates/3.0.1.yaml)
 
+cat << EOF
+interface eth0
+metric 302
+static ip_address=10.0.0.254
+static routers=10.0.0.1
+static domain_name_servers=10.0.0.1
+nolink
+
+interface wlan0
+metric 202
+EOF
